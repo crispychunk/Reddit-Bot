a2784ea3b423cebe3add7d570264af564e9fe5ae
Crispin <crispinl@sfu.ca>
2020-04-22 22:35:53 -0700

    Add run bot and login details

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
 import praw
-reddit = praw.Reddit('Bot1')
-subreddit = r.subreddit("simonfraser")
+import config
+import time
+def login():
+    
+    r = praw.Reddit(username = config.username,
+                password = config.password,
+                client_id = config.client_id,
+                client_secret = config.client_secret,
+                user_agent = "SFU RemindMe Bot v0.1")
+    return r
+    
+
+
+
+keyword = '!remindme'
+def run_bot(r):
+    subreddit = r.subreddit("testingforbots123")
+    for comment in subreddit.stream.comments():
+        if keyword in comment.body:
+            analyze_bot(comment)
+    
+
+def analyze_bot(comment):
+    # Remove remind me
+    text = comment.body
+    text = text.replace('!remindme ','')
+
+    # Now analyze when to reply
+
+    
+    #if ' Year ' in text:
+    #if ' Months ' in text:
+    #if ' Week ' in text:
+    #if 'Day' in text:
+    if 'hour' in text:
+        print(text)
+    #if 'Minute'in text:
+    #if 'EOY' in text:
+    #if 'EOM' in text:
+    #if 'EOD' in text:
+
+
+    # Now find message and store it
+    
+
+def reply_bot(time,message,comment):
+
+    
+        
+# main functions
+r = login()
+run_bot(r)
