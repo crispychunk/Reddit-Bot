35ef9d9378bdd01272b65eb38f0a73f042ced3c3
crispychunk <56182053+crispychunk@users.noreply.github.com>
2020-04-25 17:09:32 -0700

    Update config.py
    Removing information

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = 'crispychunk123'
-password = 'pBpDJ2*fa,CKMPJ'
-client_id = 'igpwQ-nqZByQPA'
-client_secret = 'P3MtUSS30ar6qgkGxB1xGcd8Wak'
+username = ''
+password = ''
+client_id = ''
+client_secret = ''
